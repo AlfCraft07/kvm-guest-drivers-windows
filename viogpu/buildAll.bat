972816021eafce860cf6af200f299d5be05f35f4
AlfCraft07 <132297876+AlfCraft07@users.noreply.github.com>
2024-07-21 17:57:10 +0200

    Update buildAll.bat
    Signed-off-by: AlfCraft07 <132297876+AlfCraft07@users.noreply.github.com>

--- viogpu/buildAll.bat
+++ viogpu/buildAll.bat
@@ -1,4 +1,4 @@
 @echo off
-call ..\tools\build.bat viogpu.sln "Win7 Win8 Win10" %*
+call ..\tools\build.bat viogpu.sln "Win7 Win8 Win8.1 Win10" %*
 if errorlevel 1 goto :eof
 call ..\tools\build.bat viogpudo\viogpudo.vcxproj "Win10_SDV" %*
